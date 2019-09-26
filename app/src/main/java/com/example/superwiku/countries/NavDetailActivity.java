3e3e1f0988ec38b0867cc603c3fb9e2bc4770ea0
superwiku <superwiku@gmail.com>
2019-09-26 13:44:22 +0700

    step 5 branch3

--- app/src/main/java/com/example/superwiku/countries/NavDetailActivity.java
+++ app/src/main/java/com/example/superwiku/countries/NavDetailActivity.java
@@ -100,7 +100,7 @@ public class NavDetailActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.nav_detail, menu);
-        return true;
+        return false;
     }
 
     @Override
